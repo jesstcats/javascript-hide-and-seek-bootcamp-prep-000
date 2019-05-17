91d8e2ed25f9bbacc525ebdb92aeef5fa27cba22
jessica cats <jesstcats@gmail.com>
2019-05-17 20:53:59 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,44 +1,23 @@
-describe('index', () => {
-  describe('getFirstSelector(selector)', () => {
-    it('returns the first element that matches the selector', () => {
-      expect(getFirstSelector('div').id).to.equal('nested')
-      expect(getFirstSelector('.ranked-list')).to.equal(document.querySelector('.ranked-list'))
-    })
-  })
+function getFirstSelector(selector){
+  return document.querySelector(selector)
 
-  describe('nestedTarget()', () => {
-    it('pulls a .target out of #nested', () => {
-      expect(nestedTarget()).to.equal(document.querySelector('#nested .target'))
-    })
-  })
+ }
 
-  describe('deepestChild()', () => {
-    it('returns the most deeply nested child in #grand-node', () => {
-      console.log(deepestChild().innerHTML)
-      expect(deepestChild()).to.equal(document.querySelector('#grand-node div div div div'))
-    })
-  })
+ function nestedTarget(){
+  return document.getElementById('nested').querySelector('div.target')
 
-  describe('increaseRankBy(n)', () => {
-    it('increases ranks in .ranked-list by n', () => {
-      increaseRankBy(3)
+ }
+function increaseRankBy(n){
+  const upRank = document.getElementById('app').querySelectorAll('ul.ranked-list')
 
-      const rankedLists = document.querySelectorAll('.ranked-list')
-      const firstList = rankedLists[0]
-      const secondList = rankedLists[1]
+   for (let i = 0; i < upRank.length; i++){
+    upRank[i].innerHTML = ((parseInt(upRank[i].innerHTML)) + n)
+    }
 
-      let children = firstList.children
-      let start = 1
-      for (let i = 0, l = children.length; i < l; i++) {
-        expect(parseInt(children[i].innerHTML)).to.equal(start + i + 3)
-      }
+ }
 
-      children = secondList.children
-      start = 12
+ function deepestChild(){
+  var deep = document.getElementById('grand-node').querySelectorAll('div')
+  return deep[deep.length-1]
 
-      for (let i = 0, l = children.length; i < l; i++) {
-        expect(parseInt(children[i].innerHTML)).to.equal(start - i + 3)
-      }
-    })
-  })
-})
+ }
